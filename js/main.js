a5f06f4cf5d03272b3e00cf281f9c8393da5a1f3
Dohun9196 <68097610+dohun9196@users.noreply.github.com>
2023-07-18 17:28:29 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -4,24 +4,29 @@ $(function () {
     $(".Side_menu .drop_nav li.fr_load").addClass("nav_on");
   }
 
-  // li 클릭 이벤트 (add,remove(.nav_on))
-  // react 제작시 <NavLink></NavLink> 태그 사용
-  $(".Side_menu .drop_nav li").on("click", function () {
+  $(".Header .gnb ._menu .nav_list .nav_tit").on("click", function () {
+    // 현재 클릭된 .nav_tit 요소와 그 부모 .nav_list 요소에 nav_on 클래스 추가
+    $(this).toggleClass("nav_on").parent().toggleClass("nav_on");
+
+    // 다른 .nav_list 요소와 그 자식 .nav_tit 요소의 nav_on 클래스 제거
     $(this)
-      .addClass("nav_on")
+      .parent()
       .siblings()
       .removeClass("nav_on")
-      .closest(".Side_menu")
-      .find(".list")
-      .not($(this).closest(".list"))
-      .find("li")
+      .find(".nav_tit")
       .removeClass("nav_on");
   });
 
-  // 다크모드 토글
-  var check = $("input[type='checkbox']");
-  check.click(function () {
-    $("p").toggle();
-    $("body").toggleClass("mod");
+  $(".Header .gnb ._menu .nav_list .drop_down>li").on("click", function () {
+    // 클릭한 요소에 nav_on 클래스 추가
+    $(this).addClass("nav_on");
+
+    // 해당 요소의 형제 요소들에서 nav_on 클래스 제거
+    $(this).siblings().removeClass("nav_on");
+
+    // 다른 .nav_list 요소에 있는 .drop_down>li.nav_on 클래스 제거
+    $(".Header .gnb ._menu .nav_list .drop_down>li.nav_on")
+      .not(this)
+      .removeClass("nav_on");
   });
 });
